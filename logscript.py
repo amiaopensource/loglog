2817ec26098be61ebd65b122c1b09eb752ff344b
Morgan <morgan@mmorel-mbp.local>
2021-04-01 21:38:03 -0700

    fixed filenames. can't have a script called logging.py or it conflicts with the logging libraries

---
+++ logscript.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
+logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
