7a46607e9927539ccab0ba5821b4f3dd2a525be9
iamdamosuzuki <ultraworld@Ultras-iMac.local>
2021-04-13 18:57:30 -0700

    not sure what these updates are

--- audio_norm.sh
+++ audio_norm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+IFS=$'\n'                                   #this is necessary to deal with spaces in filepath. (found a better way to do this so I commented it out to be safe)\
 source `dirname "$0"`/bash_logging.config    #this sets the path for the config file, which should be nested next to the script
 logDir=`pwd` # The log will be created at the working directory
 logName='log'  # The script will be named log.log
@@ -12,9 +13,10 @@ logNewLine "Starting Audio Normalization Script"
 
 filename=$(basename -- "${1}")
 extension="${filename##*.}"
+newLogLine "Testing for fileype (audio or video)"
 logNewLine "Detecting max volume for ${filename}......."
 maxVolume=$(ffmpeg -hide_banner -i "${1}" -af "volumedetect" -vn -sn -dn -f null - 2>&1 | grep 'max_volume' | awk '{print $(NF-1)}')
-logAddLine "Complete! Max volume is: ${maxVolume}"
+logAddToLine "Complete! Max volume is: ${maxVolume} dB"
 volumeBoost="${maxVolume:1}"
 audioCodec=$(ffprobe "${1}" 2>&1 >/dev/null |grep Stream.*Audio | sed -e 's/.*Audio: //' -e 's/[, ].*//')
 
